142cd1e68be18eaf399a80d74997d16d337c5960
bogdan <bogdan.oloeriu@gmail.com>
2026-03-21 11:05:39 +0100

    Initial commit for DungeonEscape3 with LFS tracking

---
+++ DungeonEscape3/Source/DungeonEscape3/DungeonEscape3.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "DungeonEscape3.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, DungeonEscape3, "DungeonEscape3" );
+
+DEFINE_LOG_CATEGORY(LogDungeonEscape3)
---
+++ DungeonEscape3/Source/DungeonEscape3/DungeonEscape3GameMode.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "DungeonEscape3GameMode.h"
+
+ADungeonEscape3GameMode::ADungeonEscape3GameMode()
+{
+	// stub
+}
