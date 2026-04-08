fee82b06134fef1cf15cb16351cec9fedd3eafb5
bogdan <bogdan.oloeriu@gmail.com>
2026-04-08 23:06:28 +0200

    added BasePawn

--- BattleBlasterStarterProject/Source/BattleBlaster/BasePawn.cpp
+++ BattleBlasterStarterProject/Source/BattleBlaster/BasePawn.cpp
@@ -9,6 +9,14 @@ ABasePawn::ABasePawn()
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	CapsuleComp = CreateDefaultSubobject<UCapsuleComponent>(TEXT("CapsuleComp"));
+	SetRootComponent(CapsuleComp);
+	
+	BaseMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("BaseMesh"));
+	BaseMesh->SetupAttachment(CapsuleComp);
+	
+	TurretMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("TurretMesh"));
+	TurretMesh->SetupAttachment(BaseMesh);
 }
 
 // Called when the game starts or when spawned
