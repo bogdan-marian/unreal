0142b56647423e1461eecdfcdb0ad65f1ad1c4e2
bogdan <bogdan.oloeriu@gmail.com>
2026-04-10 11:56:01 +0200

    added tower class

---
+++ BattleBlasterStarterProject/Source/BattleBlaster/Tower.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BasePawn.h"
+#include "Tank.h"
+#include "Tower.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLEBLASTER_API ATower : public ABasePawn
+{
+	GENERATED_BODY()
+	
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	
+public:
+	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+	
+	
+	ATank* Tank;
+};
