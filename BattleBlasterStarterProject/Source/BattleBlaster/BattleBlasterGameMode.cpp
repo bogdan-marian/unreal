7a3fa34de88598e7368364e97f9ee49bb25d7187
bogdan <bogdan.oloeriu@gmail.com>
2026-04-10 12:18:09 +0200

    counted the towers

--- BattleBlasterStarterProject/Source/BattleBlaster/BattleBlasterGameMode.cpp
+++ BattleBlasterStarterProject/Source/BattleBlaster/BattleBlasterGameMode.cpp
@@ -2,4 +2,26 @@
 
 
 #include "BattleBlasterGameMode.h"
+#include "Kismet/GameplayStatics.h"
+#include "Tank.h"
+#include "Tower.h"
 
+void ABattleBlasterGameMode::BeginPlay()
+{
+	Super::BeginPlay();
+
+	TArray<AActor*> Towers;
+	UGameplayStatics::GetAllActorsOfClass(GetWorld(), ATower::StaticClass(), Towers);
+	TowerCount = Towers.Num();
+	UE_LOG(LogTemp, Display, TEXT("Tower Count: %d"), TowerCount);
+	
+	APawn* PlayerPawn =	UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
+	if (PlayerPawn)
+	{
+		Tank = Cast<ATank>(PlayerPawn);
+		if (!Tank)
+		{
+			UE_LOG(LogTemp, Error, TEXT("Player Pawn is not a Tank"));
+		}
+	}
+}
