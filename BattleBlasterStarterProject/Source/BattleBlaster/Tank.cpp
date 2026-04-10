b2760c866d5cf0397c54e67009ae0d6f972709e2
bogdan <bogdan.oloeriu@gmail.com>
2026-04-10 09:34:54 +0200

    fixed default mappings

--- BattleBlasterStarterProject/Source/BattleBlaster/Tank.cpp
+++ BattleBlasterStarterProject/Source/BattleBlaster/Tank.cpp
@@ -21,36 +21,18 @@ void ATank::BeginPlay()
 {
 	Super::BeginPlay();
 
-	if (bool MyBool = true)
-	{
-		UE_LOG(LogTemp, Display, TEXT("MyBool is true"));
-	}
-	
 	// 2. Add the Mapping Context
 	if (APlayerController* PlayerController = Cast<APlayerController>(Controller))
 	{
-		UE_LOG(LogTemp, Display, TEXT("PlayerController is not null"));
 		if (ULocalPlayer* LocalPlayer = PlayerController->GetLocalPlayer())
 		{
-			UE_LOG(LogTemp, Display, TEXT("LocalPlayer is not null"));
-			if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(LocalPlayer))
+			if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<
+				UEnhancedInputLocalPlayerSubsystem>(LocalPlayer))
 			{
-				UE_LOG(LogTemp, Display, TEXT("Subsystem is not null"));
-				if (DefaultMappingContext == nullptr)
-				{
-					UE_LOG(LogTemp, Display, TEXT("DefaultMappingContext is null"));
-				}
-				else
-				{
-					UE_LOG(LogTemp, Display, TEXT("DefaultMappingContext is not null"));
-				}
 				Subsystem->AddMappingContext(DefaultMappingContext, 0);
-				UE_LOG(LogTemp, Display, TEXT("Subsystem->AddMappingContext completed!"));
 			}
 		}
 	}
-
-	
 }
 
 
@@ -64,17 +46,11 @@ void ATank::Tick(float DeltaTime)
 void ATank::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
-	UE_LOG(LogTemp, Display, TEXT("SetupPlayerInputComponent!"));
-    
+
 	if (UEnhancedInputComponent* EIC = Cast<UEnhancedInputComponent>(PlayerInputComponent))
 	{
-		UE_LOG(LogTemp, Display, TEXT("EIC is not null"));
 		EIC->BindAction(MoveAction, ETriggerEvent::Triggered, this, &ATank::MoveInput);
-		UE_LOG(LogTemp, Display, TEXT("BindAction completed!"));
 	}
-
-    
-	
 }
 
 void ATank::MoveInput()
