02e06775bf656bc1c46fcbb4b3ea78b52e9ae0ca
bogdan <bogdan.oloeriu@gmail.com>
2026-04-12 11:44:04 +0200

    added niagara

--- BattleBlasterStarterProject/Source/BattleBlaster/BattleBlaster.Build.cs
+++ BattleBlasterStarterProject/Source/BattleBlaster/BattleBlaster.Build.cs
@@ -9,7 +9,7 @@ public class BattleBlaster : ModuleRules
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
 		PublicDependencyModuleNames.AddRange(new string[]
-			{ "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "UMG" });
+			{ "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "UMG", "Niagara" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] { });
 
