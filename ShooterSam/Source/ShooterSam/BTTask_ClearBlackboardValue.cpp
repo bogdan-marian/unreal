64d35946b9d92bef1832e5b63707edb9d1eec821
bogdan <bogdan.oloeriu@gmail.com>
2026-04-18 18:25:11 +0200

    fixed chase

---
+++ ShooterSam/Source/ShooterSam/BTTask_ClearBlackboardValue.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BTTask_ClearBlackboardValue.h"
+#include "BehaviorTree/BlackboardComponent.h"
+
+UBTTask_ClearBlackboardValue::UBTTask_ClearBlackboardValue()
+{
+	NodeName = "Clear Blackboard Value";
+}
+
+EBTNodeResult::Type UBTTask_ClearBlackboardValue::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	Super::ExecuteTask(OwnerComp, NodeMemory);
+	
+	// get the blackboard comp
+	UBlackboardComponent* BlackboardComp = OwnerComp.GetBlackboardComponent();
+	
+	// clear the value of selected key
+	if (BlackboardComp)
+	{
+		BlackboardComp->ClearValue(GetSelectedBlackboardKey());
+	}
+	
+	// return success or failure
+	return EBTNodeResult::Succeeded;
+}
