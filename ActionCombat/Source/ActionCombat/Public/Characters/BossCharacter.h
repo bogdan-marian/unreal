8d60dfa5ef0a7330641fef2fb0f43dc43bfc8097
bogdan <bogdan.oloeriu@gmail.com>
2026-04-19 23:47:38 +0200

    added interfaces

--- ActionCombat/Source/ActionCombat/Public/Characters/BossCharacter.h
+++ ActionCombat/Source/ActionCombat/Public/Characters/BossCharacter.h
@@ -4,10 +4,11 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "Interfaces/Enemy.h"
 #include "BossCharacter.generated.h"
 
 UCLASS()
-class ACTIONCOMBAT_API ABossCharacter : public ACharacter
+class ACTIONCOMBAT_API ABossCharacter : public ACharacter, public IEnemy
 {
 	GENERATED_BODY()
 
