6496e36bfcaeb1e7b7222b2701c5dc5e23408ecf
bogdan <bogdan.oloeriu@gmail.com>
2026-04-23 22:03:38 +0200

    anamy rotates to us

---
+++ ActionCombat/Source/ActionCombat/Public/Characters/LookAtPlayerComponent.h
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "LookAtPlayerComponent.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class ACTIONCOMBAT_API ULookAtPlayerComponent : public UActorComponent
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(EditAnywhere)
+	float Speed{400.0f};
+
+public:	
+	// Sets default values for this component's properties
+	ULookAtPlayerComponent();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+		
+};
