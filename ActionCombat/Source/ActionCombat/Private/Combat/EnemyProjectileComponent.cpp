e2b694fa41e01849779b9a87471d6961e2553f01
bogdan <bogdan.oloeriu@gmail.com>
2026-04-23 21:29:00 +0200

    projectile spawn location

--- ActionCombat/Source/ActionCombat/Private/Combat/EnemyProjectileComponent.cpp
+++ ActionCombat/Source/ActionCombat/Private/Combat/EnemyProjectileComponent.cpp
@@ -20,15 +20,24 @@ void UEnemyProjectileComponent::BeginPlay()
 	Super::BeginPlay();
 
 	// ...
-	
 }
 
 
 // Called every frame
-void UEnemyProjectileComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+void UEnemyProjectileComponent::TickComponent(float DeltaTime, ELevelTick TickType,
+                                              FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
 	// ...
 }
 
+void UEnemyProjectileComponent::SpawnProjectile(FName ComponentName, TSubclassOf<AActor> ProjectileClass)
+{
+	USceneComponent* SpawnPointComp = Cast<USceneComponent>(
+		GetOwner()->GetDefaultSubobjectByName(ComponentName));
+
+	FVector SpawnLocation{SpawnPointComp->GetComponentLocation()};
+
+	GetWorld()->SpawnActor(ProjectileClass, &SpawnLocation);
+}
