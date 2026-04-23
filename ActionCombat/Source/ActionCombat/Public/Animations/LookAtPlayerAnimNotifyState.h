566d72dd260892be531a1e0b7e64ec1ef9d1f220
bogdan <bogdan.oloeriu@gmail.com>
2026-04-23 22:07:24 +0200

    added LookAtPlayerAnimNotifyState and its header

---
+++ ActionCombat/Source/ActionCombat/Public/Animations/LookAtPlayerAnimNotifyState.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimNotifies/AnimNotifyState.h"
+#include "LookAtPlayerAnimNotifyState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ACTIONCOMBAT_API ULookAtPlayerAnimNotifyState : public UAnimNotifyState
+{
+	GENERATED_BODY()
+	
+};
