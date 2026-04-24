80b7b0ebf3e3891cdbf5a4fc00fa76d202f80238
bogdan <bogdan.oloeriu@gmail.com>
2026-04-24 18:44:24 +0200

    boss started chasing

--- ActionCombat/Source/ActionCombat/Private/Characters/AI/BTT_MeleeAttack.cpp
+++ ActionCombat/Source/ActionCombat/Private/Characters/AI/BTT_MeleeAttack.cpp
@@ -2,9 +2,26 @@
 
 
 #include "Characters/AI/BTT_MeleeAttack.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "AIController.h"
+#include "Navigation/PathFollowingComponent.h"
 
 EBTNodeResult::Type UBTT_MeleeAttack::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {
+	float Distance{
+		OwnerComp.GetBlackboardComponent()->GetValueAsFloat(TEXT("Distance"))
+	};
+
+	if (Distance > AttackRadius)
+	{
+		APawn* PlayerRef = GetWorld()->GetFirstPlayerController()->GetPawn();
+		FAIMoveRequest MoveRequest{PlayerRef};
+		MoveRequest.SetUsePathfinding(true);
+		MoveRequest.SetAcceptanceRadius(AcceptableRadius);
+		OwnerComp.GetAIOwner()->MoveTo(MoveRequest);
+		OwnerComp.GetAIOwner()->SetFocus(PlayerRef);
+	}
+
 	return EBTNodeResult::InProgress;
 }
 
--- ActionCombat/Source/ActionCombat/Public/Characters/AI/BTT_MeleeAttack.h
+++ ActionCombat/Source/ActionCombat/Public/Characters/AI/BTT_MeleeAttack.h
@@ -13,15 +13,20 @@ UCLASS()
 class ACTIONCOMBAT_API UBTT_MeleeAttack : public UBTTaskNode
 {
 	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere)
+	float AttackRadius{200.0f};
+
 	
+	UPROPERTY(EditAnywhere)
+	float AcceptableRadius{100.0f};
 protected:
 	virtual EBTNodeResult::Type ExecuteTask(
 		UBehaviorTreeComponent& OwnerComp,
 		uint8* NodeMemory) override;
-	
+
 	virtual void TickTask(
 		UBehaviorTreeComponent& OwnerComp,
-		uint8* NodeMemory, 
+		uint8* NodeMemory,
 		float DeltaSeconds) override;
-	
 };
